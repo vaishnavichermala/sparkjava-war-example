535c08d05f853137f15574837d23d0b07beced91
vaishnavichermala <92431369+vaishnavichermala@users.noreply.github.com>
2021-11-13 17:31:15 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello World checking jenkins by vaishnavi");
+		get("/hello", (req, res) -> "Hello World");
 	}
 }
